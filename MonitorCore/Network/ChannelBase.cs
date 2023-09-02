9e3d7092a1b7f817b562850f0f546668e8bc9ff3
寬寬 <polu635242@gmail.com>
2023-09-02 20:46:55 +0800

    支援序列化與反序列化模組

---
+++ MonitorCore/Network/ChannelBase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitorCore
+{
+    public class ChannelBase
+    {
+        public ChannelBase () 
+        {
+            networkRouter.BindingOnMsg (OnMsg);
+        }
+
+        public void BindEvent<T> (int eventID, Action<T> callback) where T:new ()
+        {
+            if (cacheEvents.Exists (pair => pair.eventID == eventID))
+            {
+                LoggerRouter.WriteLine ($"註冊重覆的ID -> {eventID}");
+            }
+
+            Action<string> proxy = (string str) =>
+            {
+                T data = JsonUtility.FromJson<T> (str);
+                callback.Invoke (data);
+            };
+
+            cacheEvents.Add ((eventID, proxy));
+        }
+
+        
+
+        List<(int eventID, Action<string> callback)> cacheEvents = new List<(int eventID, Action<string> callback)> ();
+
+        void OnMsg (string msg) 
+        {
+            MsgContainer msgContainer = JsonUtility.FromJson<MsgContainer> (msg);
+
+            var eventIndex = cacheEvents.FindIndex (pair => pair.eventID == msgContainer.eventID);
+
+            if (eventIndex >= 0)
+            {
+                cacheEvents[eventIndex].callback.Invoke (msgContainer.json);
+            }
+            else
+            {
+                LoggerRouter.WriteLine ($"沒有被註冊的ID -> {msgContainer.eventID}");
+            }
+        }
+
+        NetworkRouter networkRouter = new NetworkRouter ();
+
+        /// <summary>
+        /// 轉傳的依據
+        /// </summary>
+        [Serializable]
+        internal class MsgContainer
+        {
+            public int eventID;
+            public string json;
+        }
+    }
+}
---
+++ MonitorCore/Network/NetworkRouter.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitorCore
+{
+    public class NetworkRouter
+    {
+        readonly int uintSize = sizeof (uint);
+
+        public void ReceiveMsg (byte[] msg) 
+        {
+            buffer.AddRange (msg);
+
+            ProcessBuffer ();
+        }
+
+        public void BindingOnMsg (Action<string> onMsg) 
+        {
+            this.onMsg = onMsg;
+        }
+
+        Action<string> onMsg;
+
+        void ProcessBuffer () 
+        {
+            //找出header
+            if (bufferStatus == BufferStatus.WaitingHeader)
+            {
+                if (buffer.Count >= uintSize)
+                {
+                    bufferStatus = BufferStatus.WaitingMsg;
+
+                    var headerBuffer = new byte[uintSize];
+
+                    buffer.CopyTo (0, headerBuffer, 0, uintSize);
+
+                    headerSize = BitConverter.ToInt32 (headerBuffer, 0);
+
+                    var deltaSize = buffer.Count - uintSize;
+
+                    //還有剩
+                    if (deltaSize > 0)
+                    {
+                        byte[] newBuffer = new byte[deltaSize];
+
+                        //第一個參數是指從哪個index開始複製,
+                        //第四個參數只要複製多長
+                        buffer.CopyTo (uintSize, newBuffer, 0, deltaSize);
+
+                        buffer = newBuffer.ToList ();
+
+                        if (deltaSize > headerSize)
+                        {
+                            ProcessBuffer ();
+                        }
+                    }
+                }
+            }
+            else if (bufferStatus == BufferStatus.WaitingMsg)
+            {
+                if (buffer.Count >= headerSize)
+                {
+                    bufferStatus = BufferStatus.WaitingHeader;
+
+                    var msgBuffer = new byte[headerSize];
+
+                    buffer.CopyTo (0, msgBuffer, 0, headerSize);
+
+                    string msg = encoding.GetString (msgBuffer);
+
+                    if (onMsg != null)
+                    {
+                        try
+                        {
+                            onMsg.Invoke (msg);
+                        }
+                        catch(Exception e)
+                        {
+                            LoggerRouter.WriteLine (e.Message);
+                            LoggerRouter.WriteLine (e.StackTrace);
+                        }
+                    }
+
+                    var deltaSize = buffer.Count - headerSize;
+
+                    //還有剩
+                    if (deltaSize > 0)
+                    {
+                        byte[] newBuffer = new byte[deltaSize];
+
+                        //第一個參數是指從哪個index開始複製,
+                        //第四個參數只要複製多長
+                        buffer.CopyTo (headerSize, newBuffer, 0, deltaSize);
+
+                        buffer = newBuffer.ToList ();
+
+                        if (deltaSize > uintSize)
+                        {
+                            ProcessBuffer ();
+                        }
+                    }
+                }
+            }
+        }
+
+        Encoding encoding = Encoding.UTF8;
+
+        int headerSize;
+
+        BufferStatus bufferStatus = BufferStatus.WaitingHeader;
+
+        List<byte> buffer;
+
+        public byte[] GetMsgBuffers<T> (T msg) 
+        {
+            List<byte> msgBuffers = new List<byte> ();
+
+            var json = JsonUtility.ToJson (msg);
+
+            var jsonBuffer = encoding.GetBytes (json);
+
+            //檔頭等於後續json的長度
+            var header = (uint)jsonBuffer.Length;
+            var headerBuffer = BitConverter.GetBytes (header);
+            msgBuffers.AddRange (headerBuffer);
+            msgBuffers.AddRange (jsonBuffer);
+
+            return msgBuffers.ToArray ();
+        }
+
+        enum BufferStatus 
+        {
+            WaitingHeader,
+            WaitingMsg
+        }
+    }
+}
